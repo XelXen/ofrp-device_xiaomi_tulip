4454a5b9d0fcad55be7fdd694fb2109b71e60dcd
XelXen <xelte.xynos@gmail.com>
2021-09-09 06:38:20 +0530

    Ensure that Build vars get exported

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,7 +17,24 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
+
 FDEVICE="tulip"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export PLATFORM_VERSION="16.1.0"
    	export TW_DEFAULT_LANGUAGE="en"
@@ -37,12 +54,17 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"
 	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+
+	# ! keeping dm-verity is important, else you will be spammed by MIUI about your device
+	# export OF_KEEP_DM_VERITY="1" 
 
 	# OTA for custom ROMs
         export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
         export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
         # -- add settings for R11 --
+        export FOX_R11=1
         export FOX_ADVANCED_SECURITY=1
         export OF_USE_TWRP_SAR_DETECT=1
         export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
@@ -58,7 +80,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	fi
 
    	for var in eng userdebug; do
-       	    add_lunch_combo omni_"$FDEVICE"-$var
+       	    add_lunch_combo omni_"$FDEVICE"-eng
    	done
 fi
 #
