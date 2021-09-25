de91fb0ea1fe77ac05f4f5bf03edad6a22a29b81
XelXen <xelte.xynos@gmail.com>
2021-09-25 16:57:33 +0530

    New stuff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -70,6 +70,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
         export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
         # -- end R11 settings --
+	
+	export OF_MAINTAINER_AVATAR="misc/avatar.png"
+	export FOX_USE_SPECIFIC_MAGISK_ZIP="misc/Magisk.zip"	
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
